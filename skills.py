2f6bc73bc272c17543553b8793699beb660af248
Angela Crepaldi <angelacrepaldi@gmail.com>
2020-02-26 12:16:41 -0300

    Created method POST for class Skills to add a new skill to the list.

--- skills.py
+++ skills.py
@@ -1,4 +1,6 @@
 from flask_restful import Resource
+from flask import request
+import json
 
 
 list_skills = ['Python', 'Django', 'Flask', 'Java', 'C']
@@ -7,3 +9,10 @@ list_skills = ['Python', 'Django', 'Flask', 'Java', 'C']
 class Skills(Resource):
     def get(self):
         return list_skills
+
+    def post(self):
+        data_in = json.loads(request.data)
+        skill = data_in['NewSkill']
+        list_skills.append(skill)
+        message = "New skill inserted: {}".format(skill)
+        return {"status": "Success!", "message": message}
