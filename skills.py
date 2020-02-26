4c0886b2ac48a0a849edc6851e481ce9ac1174b8
Angela Crepaldi <angelacrepaldi@gmail.com>
2020-02-26 12:38:13 -0300

    Created method PUT for class Skills. Added example for POST.

--- skills.py
+++ skills.py
@@ -11,8 +11,23 @@ class Skills(Resource):
         return list_skills
 
     def post(self):
+        # Example data_in:
+        # {"NewSkill": "Assembly"}
         data_in = json.loads(request.data)
         skill = data_in['NewSkill']
         list_skills.append(skill)
         message = "New skill inserted: {}".format(skill)
+        return {"status": "Success!", "message": message}
+
+    def put(self):
+        # Example data_in:
+        # {
+        #     "id": 1,
+        #     "skill": "Ada"
+        # }
+        data_in = json.loads(request.data)
+        position = data_in["id"]
+        skill = data_in["skill"]
+        list_skills[position] = skill
+        message = "Edited skill n. {}.".format(position)
         return {"status": "Success!", "message": message}
